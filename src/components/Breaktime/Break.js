5d978be729fb94b0a9133da3747867d9df0a5907
anisa6121 <you@example.com>
2022-09-29 22:24:52 +0600

    done

--- src/components/Breaktime/Break.js
+++ src/components/Breaktime/Break.js
@@ -1,37 +1,88 @@
-
 import React, { useEffect, useState } from "react";
 
 import "./Break.css";
 
 const Break = () => {
+	const [time, settime] = useState(0);
 
- 	const [time, settime] = useState(0)
-	
-	
-localStorage.setItem('time', JSON.stringify(settime))
-    return (
+useEffect(() => {
+const times = JSON.parse(localStorage.getItem("time"));
+
+if (times) {
+	settime(times);
+	}
+	}, []);
+
+	return (
 		<div className="">
 			<div className="clicked-btn">
-				<button className="btn" onClick={() => settime(10)}>10s</button>
-				<button className="btn" onClick={() => settime(20)}>20s</button>
-				<button className="btn" onClick={() => settime(30)}>30s</button>
-				<button className="btn" onClick={() => settime(40)}>40s</button>
-				<button className="btn" onClick={() => settime(50)}>50s</button>
+				<button
+					className="btn"
+					onClick={() => {
+						settime(10);
 
-				
-			</div>
+						localStorage.setItem(
+							"time",
+							JSON.stringify(10)
+						);
+					}}
+				>
+					10s
+				</button>
 
-			<div className="detail">
-                <h3> Exercise Details</h3>
-                
-            <div className="break-time">
-			<h3>Break Time: {time}seconds </h3>
-			</div>
-			</div>
+				<button
+					className="btn"
+					onClick={() => {
+						settime(20);
+						localStorage.setItem(
+							"time",
+							JSON.stringify(20)
+						);
+					}}
+				>
+					20s
+				</button>
 
+				<button
+					className="btn"
+					onClick={() => {
+						settime(30);
+						localStorage.setItem(
+							"time",
+							JSON.stringify(30)
+						);
+					}}
+				>
+					30s
+				</button>
+
+				<button
+					className="btn"
+					onClick={() => {
+						settime(40);
+						localStorage.setItem(
+							"time",
+							JSON.stringify(40)
+						);
+					}}
+				>
+					40s
+				</button>
+
+<button className="btn"onClick={() => {settime(50);
+localStorage.setItem("time",JSON.stringify(50));}}>50s
+</button>
+			</div>
 			
+<div className="detail">
+				<h3> Exercise Details</h3>
+
+				<div className="break-time">
+					<h3>Break Time: {time}seconds </h3>
+				</div>
+			</div>
 		</div>
-    );
+	);
 };
 
 export default Break;
